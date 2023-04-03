31cdd617cf398849898e471672c04eaec6095416
Dave Masino <davem@slalom.com>
2023-04-03 13:33:00 -0700

    Use taxi data for analytics query

--- snowflake/query_analytics.sql
+++ snowflake/query_analytics.sql
@@ -1,17 +1,15 @@
--- Airport departure delays by quarter
--- ORD selected
-
 use role reporting;
 use warehouse analyze_wh;
 use database analytics;
-
+ 
+-- Average and max tip by quarter
+-- What else can you discover?
 select 
 d.quarter_name_full
-,avg(a.dep_delay) as avg_delay
-,max(a.cal_date) as cal_date_sort
-from airline a 
-inner join calendar_date d on a.cal_date = d.cal_date
-where d.year between 2004 and 2008
-and origin='ORD'
+,avg(tip_amount) as avg_tip
+,max(tip_amount) as max_tip
+,max(d.cal_date) as cal_date_sort
+from nyc_taxi t
+inner join calendar_date d on t.tpep_pickup_date = d.cal_date
 group by quarter_name_full
 order by cal_date_sort asc;
